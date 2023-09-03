0760c5886ef7f5e19a1afffd1a3e57135ff79fd5
Dennis Dietrich <d.dietrich84@gmail.com>
2023-09-03 19:28:48 +0200

    kotlin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,17 @@ return {
 		"neovim/nvim-lspconfig",
 		opts = {
 			autoformat = false,
+			servers = {
+				kotlin_language_server = {},
+			},
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"kotlin-language-server",
+			},
 		},
 	},
 }
