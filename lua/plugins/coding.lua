cb40bfeef95de51ebcd7c513c3d1c97f19540e19
Dennis Dietrich <d.dietrich84@gmail.com>
2023-09-03 14:29:16 +0200

    rust

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -8,4 +8,22 @@ return {
 			require("retrail").setup()
 		end,
 	},
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "Saecki/crates.nvim",
+        event = { "BufRead Cargo.toml" },
+        config = true,
+      },
+    },
+    -- @param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "crates" },
+      }))
+    end,
+  }
 }
