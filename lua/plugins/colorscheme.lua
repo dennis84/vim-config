c50def1c3a0d3b24059228d79046dc669af3cc44
Dennis Dietrich <d.dietrich84@gmail.com>
2024-04-24 09:36:44 +0200

    switch colorscheme on system darkmode

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,4 +3,19 @@ return {
 		"folke/tokyonight.nvim",
 		opts = { style = "moon" },
 	},
+	{
+		"f-person/auto-dark-mode.nvim",
+		opts = {
+			update_interval = 1000,
+			set_dark_mode = function()
+				vim.cmd("colorscheme tokyonight-moon")
+			end,
+			set_light_mode = function()
+				vim.cmd("colorscheme tokyonight-day")
+			end,
+		},
+		init = function()
+			require("auto-dark-mode").init()
+		end,
+	},
 }
